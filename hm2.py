6c873b54ef3695dd672dd46f569990b95cc27d33
Sardor <sardor.bobojonov@gmail.com>
2023-01-02 01:05:23 +0500

    initial commit

---
+++ hm2.py
@@ -0,0 +1,33 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+# x y z                     ⋁ - dizyunkciya ⋀ - konyukciya  ¬ - otricanie
+# 0 0 0
+# 0 0 1
+# 0 1 0
+# 0 1 1
+# 1 0 0
+# 1 0 1
+# 1 1 0
+# 1 1 1
+
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
